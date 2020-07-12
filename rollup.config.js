5bd98dff60c0f3c0c2fdcc12c6f7619098694150
Guilherme Belmonte <contact@belmonte.dev>
2020-07-12 00:36:24 -0300

    Change to typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,16 @@
 import run from '@rollup/plugin-run';
+import typescript from 'rollup-plugin-typescript'
 
 const dev = process.env.NODE_ENV !== 'production';
 
 export default {
-  input: 'src/main.js',
+  input: 'src/main.ts',
   output: {
     file: 'dist/main.js',
     format: 'cjs',
   },
   plugins: [
+    typescript(),
     dev && run(),
   ],
 };
